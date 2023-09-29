b0fdba5a9bd8abf865c7b3daaa9b762dc00182d3
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-09-29 16:59:47 -0300

    feat: adiciona logo

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,7 @@
-import { VStack, Image } from 'native-base'
+import { VStack, Image, Text, Center } from 'native-base'
+
+
+import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 
 export function SingIn() {
@@ -10,6 +13,14 @@ export function SingIn() {
         resizeMode='contain'
         position={'absolute'}
       />
+
+      <Center my={24}>
+        <LogoSvg />
+        <Text color={'gray.100'} fontSize={'sm'}>
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+      
     </VStack>
   )
 }
