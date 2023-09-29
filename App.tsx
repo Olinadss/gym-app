047918db3f26445986a295595b70c7502432a9d9
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-09-29 16:47:52 -0300

    feat: cria background do login

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme'
+import { SingIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-      {fontsLoaded ? <Text>Hello Word!</Text> : <Loading />}
+      {fontsLoaded ? <SingIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
