f0cb61b9fad0e0c52daf00a7737ff6266eb1d903
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-02 14:35:27 -0300

    feat: cria estrutura de telas

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme'
 import { Routes } from '@routes/index';
+import { Home } from '@screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
