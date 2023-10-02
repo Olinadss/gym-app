d8513e7f483dc8175cae4bd172315ae72fd4c361
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-02 14:10:07 -0300

    feat: finaliza rotas de autenticação

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,19 +1,26 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 import { SingIn } from '@screens/SignIn';
 import { SingUp } from '@screens/SignUp';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+type AuthRoutes = {
+  signIn: undefined
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export function AuthRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen
         name='signIn'
         component={SingIn}
       />
       <Screen
-        name='signUn'
+        name='signUp'
         component={SingUp}
       />
     </Navigator>
