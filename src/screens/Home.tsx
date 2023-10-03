502fda8050c64d1d864546e6bb560c7ddd2bfd40
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-03 09:35:32 -0300

    feat: lista cards de exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,6 +7,7 @@ import { ExerciseCard } from '@components/ExerciseCard'
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('costa')
   const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra'])
 
   return (
     <VStack flex={1}>
@@ -34,11 +35,18 @@ export function Home() {
             Exercícios
           </Heading>
           <Text color={'gray.200'} fontSize={'sm'}>
-            4
+            {exercises.length}
           </Text>
         </HStack>
-        <ExerciseCard />
-        <ExerciseCard />
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20}}
+        />
       </VStack>
     </VStack>
   )
