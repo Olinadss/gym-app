d86194d6c6afed76f1dee6bb576c8a2212007ee4
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-03 17:53:33 -0300

    feat: finaliza tela de perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { Center, VStack, Skeleton, Text } from 'native-base'
+import { Center, VStack, Skeleton, Text, Heading } from 'native-base'
 import { useState } from 'react'
 import { ScrollView, TouchableOpacity } from 'react-native'
 
@@ -11,7 +13,7 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
-      <ScrollView>
+      <ScrollView contentContainerStyle={{paddingBottom: 36}}>
         <Center mt={6} px={10}>
           {
             photoIsLoading ?
@@ -34,6 +36,40 @@ export function Profile() {
               Alterar Foto
             </Text>
           </TouchableOpacity>
+
+          <Input 
+            bg={'gray.600'}
+            placeholder='Nome'
+          />
+          <Input
+            bg={'gray.600'}
+            placeholder='danilo.santos@gmail.com'
+            isDisabled
+          />
+          <Heading color={'gray.200'} fontSize={'md'} mb={2} alignSelf={'flex-start'} mt={12}>
+            Alterar senha
+          </Heading>
+
+          <Input 
+            bg={'gray.600'}
+            placeholder='Senha antiga'
+            secureTextEntry
+          />
+          <Input 
+            bg={'gray.600'}
+            placeholder='Nova senha'
+            secureTextEntry
+          />
+          <Input 
+            bg={'gray.600'}
+            placeholder='Confirmar nova senha'
+            secureTextEntry
+          />
+
+          <Button 
+            title='Atualizar'
+            mt={4}
+          />
         </Center>
       </ScrollView>
     </VStack>
