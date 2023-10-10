25a9c5be723843695112b8bedc08fd64539efa66
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-09 21:43:56 -0300

    feat: adiciona foto default caso usuario nao tenha foto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,8 +2,11 @@ import { Heading, VStack, Text, HStack, Icon } from 'native-base'
 import { UserPhoto } from './UserPhoto'
 import { MaterialIcons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
+import { useAuth } from '@hooks/useAuth'
+import defaultUserPhoto from '@assets/userPhotoDefault.png'
 
 export function HomeHeader() {
+	const { user } = useAuth()
 	return (
 		<HStack
 			backgroundColor={'gray.600'}
@@ -14,9 +17,13 @@ export function HomeHeader() {
 		>
 			<UserPhoto
 				size={16}
-				source={{
-					uri: 'https://github.com/olinadss.png',
-				}}
+				source={
+					user.avatar
+						? {
+								uri: user.avatar,
+						  }
+						: defaultUserPhoto
+				}
 				alt='Imagem do usuário'
 				mr={4}
 			/>
@@ -25,7 +32,7 @@ export function HomeHeader() {
 					Olá,
 				</Text>
 				<Heading color={'gray.100'} fontSize={'md'} fontFamily={'heading'}>
-					Danilo
+					{user.name}
 				</Heading>
 			</VStack>
 
