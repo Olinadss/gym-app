6641d56de49dc254ee805fc4dad8c4af073f1eb3
Danilo dos Santos <danilo.santossouza@outlook.com>
2023-10-09 21:36:48 -0300

    feat: adiciona loading ao recarregar a pagina

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -9,6 +9,7 @@ import { api } from '@services/api'
 export type AuthContextDataProps = {
 	user: UserDTO
 	signIn: (email: string, password: string) => void
+	isLoadingStorageData: boolean
 }
 
 type AuthContextProviderProps = {
@@ -21,6 +22,7 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	const [user, setUser] = useState({} as UserDTO)
+	const [isLoadingStorageData, setIsLoadingStorageData] = useState(true)
 
 	async function signIn(email: string, password: string) {
 		try {
@@ -36,10 +38,16 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	}
 
 	async function loadUserData() {
-		const userLogged = await storageUserGet()
+		try {
+			const userLogged = await storageUserGet()
 
-		if (userLogged) {
-			setUser(userLogged)
+			if (userLogged) {
+				setUser(userLogged)
+			}
+		} catch (error) {
+			throw error
+		} finally {
+			setIsLoadingStorageData(false)
 		}
 	}
 
@@ -48,7 +56,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	}, [])
 
 	return (
-		<AuthContext.Provider value={{ user, signIn }}>
+		<AuthContext.Provider value={{ user, signIn, isLoadingStorageData }}>
 			{children}
 		</AuthContext.Provider>
 	)
